5ead0567a73edbc251620ef8ef513cad93e8ba97
Pukar Chhatkuli <rockypukar@pop-os.localdomain>
2024-07-09 13:14:22 +0545

    feat: add bootstrap, enqueue css, js

--- functions.php
+++ functions.php
@@ -5,12 +5,21 @@
  * @package Aquila
  */
 function aquila_enqueue_scripts() {
+  // Register styles
   // wp_register_style(name, src, dependency, version, media(print, screen-orientation))
   wp_register_style("style-css", get_stylesheet_directory_uri(), [], filemtime(get_template_directory() . "/style.css"), "all");
+  wp_register_style("bootstrap-css", get_template_directory_uri() . "/assets/src/library/css/bootstrap.min.css", [], null, "all");
+
+  // Enqueue styles
   wp_enqueue_style("style-css");
+  wp_enqueue_style("bootstrap-css");
   
+  // Register scripts
   // wp_register_script(name, src, dependency, version, in_footer?)
   wp_register_script("main-js", get_template_directory_uri() . "/assets/main.js", [], filemtime(get_template_directory() . "/assets/main.js"), true);
+  wp_register_script("bootstrap-js", get_template_directory_uri() . "/assets/src/library/js/bootstrap.min.js", ["jquery"], false, true);
+  // Enqueue scripts
   wp_enqueue_script("main-js");
+  wp_enqueue_script("bootstrap-js");
 }
 add_action("wp_enqueue_scripts", "aquila_enqueue_scripts");
